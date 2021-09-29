43d847a53f23ad39bceeb6ed01e3a2abba6e552d
asmi046 <asmi046@gmail.com>
2021-09-29 22:51:55 +0300

    ssl

--- crm-rest.php
+++ crm-rest.php
@@ -21,12 +21,18 @@ function get_number_info($number) {
 	curl_setopt($curl, CURLOPT_URL, $url);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	// curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-	// curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-	// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 	// curl_setopt($curl, CURLOPT_SSLVERSION, 3);
 	// curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
-	curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+	// curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 	$str = curl_exec($curl);
+
+	if($str === false)
+	{
+   	 	echo 'Ошибка curl: ' . curl_error($curl);
+	}
+
 	curl_close($curl);
 
 	return json_decode($str);
